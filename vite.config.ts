053a8b0d972e2f81a50edd126a2958f6d5a98c2f
ShinjungOh <97431021+ShinjungOh@users.noreply.github.com>
2024-06-04 19:36:54 +0900

    env: 개발 환경 세팅 (#2)
    * env: 프로젝트 init
    
    * env: yarn PnP 설정
    
    * chore: 프로젝트 라이브러리 설치
    
    * chore: storybook 설치
    
    * env: husky 세팅
    
    * chore: eslint, prettier 설정
    
    * chore: vitest 설정
    
    * chore: tsconfig 설정
    
    * env: github actions ci 추가
    
    * chore: barrels 설정

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    include: ['src/**/*.test.{ts,tsx}'],
+    exclude: ['node_modules', 'dist'],
+    globals: true,
+    environment: 'jsdom',
+  },
+});
