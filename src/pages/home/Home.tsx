a8e087ee904fa314304a7e5d27eafe4e11eb5340
ShinjungOh <tlswjd9260@gmail.com>
2024-08-07 17:48:44 +0900

    feat: 노트 리스트, 노트 아이템 UI 구현

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,23 @@
+import { Box } from '@chakra-ui/react';
 import { Body } from '@/shared';
 import { Nav } from '@/shared/components/Nav';
+import { NoteList } from '@/shared/components/NoteList';
 
 export const Home = () => (
   <>
     <Nav />
     <Body>
-      <h1>home</h1>
+      <Box
+        style={{
+          margin: 0,
+          padding: '0 47px',
+          paddingRight: '35px',
+          width: '100%',
+        }}
+      >
+        <h1>home</h1>
+        <NoteList />
+      </Box>
     </Body>
   </>
 );
---
+++ src/shared/components/NoteList/NoteList.tsx
@@ -0,0 +1,11 @@
+import { NoteListItem } from '@/shared/components/NoteList/NoteListItem.tsx';
+
+export const NoteList = () => (
+  <div>
+    <NoteListItem />
+    <NoteListItem />
+    <NoteListItem />
+    <NoteListItem />
+    <NoteListItem />
+  </div>
+);
