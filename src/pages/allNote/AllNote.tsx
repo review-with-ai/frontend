81fe170928cab11527cf3156879324fbe9c433e3
ShinjungOh <tlswjd9260@gmail.com>
2024-08-14 22:51:52 +0900

    feat: 전체노트, 휴지통 페이지 노트 체크박스 UI 구현

--- src/pages/allNote/AllNote.tsx
+++ src/pages/allNote/AllNote.tsx
@@ -1,10 +1,16 @@
-import { Flex } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Button, Checkbox, Flex, Image, Stack } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
-import { BaseButton, Body, Nav, NoteList, PATH, Typography } from '@/shared';
+import { BaseButton, Body, Nav, NoteList, PATH, styleToken, Typography } from '@/shared';
 
 export const AllNote = () => {
   const navigate = useNavigate();
+  const [checkedItems, setCheckedItems] = useState([false, false]);
+
+  const allChecked = checkedItems.every(Boolean);
+  const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+  // https://v2.chakra-ui.com/docs/components/checkbox#indeterminate
 
   const handleNewNote = () => {
     navigate(PATH.NOTE);
@@ -30,6 +36,36 @@ export const AllNote = () => {
             <BaseButton type="button" theme="gray" onClick={handleNewNote}>
               <Typography variant="subtitle2">노트 만들기</Typography>
             </BaseButton>
+          </Flex>
+          <Flex alignItems="center" margin="0 0 1px 8px">
+            <Checkbox
+              padding="16px 14px 16px 17px"
+              isChecked={allChecked}
+              isIndeterminate={isIndeterminate}
+              onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+            />
+            <Stack direction="row" spacing={4}>
+              <Button
+                colorScheme="blackAlpha"
+                variant="outline"
+                height="32px"
+                padding="0 11px"
+                borderColor={styleToken.color.gray200}
+              >
+                <Image src="/src/assets/icon/share.svg" alt="노트 이동" width="16px" marginRight="6px" />
+                <Typography variant="subtitle1">노트 이동</Typography>
+              </Button>
+              <Button
+                colorScheme="blackAlpha"
+                variant="outline"
+                height="32px"
+                padding="0 11px"
+                borderColor={styleToken.color.gray200}
+              >
+                <Image src="/src/assets/icon/trash.svg" alt="휴지통으로 이동" width="16px" marginRight="6px" />
+                <Typography variant="subtitle1">휴지통으로 이동</Typography>
+              </Button>
+            </Stack>
           </Flex>
           <NoteList noteType="all" />
         </Container>
--- src/pages/trash/Trash.tsx
+++ src/pages/trash/Trash.tsx
@@ -1,47 +1,86 @@
-import { Flex, Image, Tag, TagLabel } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Button, Checkbox, Flex, Image, Stack, Tag, TagLabel } from '@chakra-ui/react';
 import styled from '@emotion/styled';
-import { Body, NoteList, Typography } from '@/shared';
+import { Body, NoteList, styleToken, Typography } from '@/shared';
 import { Nav } from '@/shared/components/Nav';
 
-export const Trash = () => (
-  <>
-    <Nav />
-    <Body>
-      <Container>
-        <Flex
-          flexDirection="row"
-          justify="space-between"
-          style={{
-            padding: '29px 35px 28px 47px',
-          }}
-        >
-          <Typography variant="h1" fontWeight={400}>
-            휴지통
-          </Typography>
-          <Tag
-            size="md"
-            borderRadius="full"
-            variant="outline"
-            colorScheme="blackAlpha"
+export const Trash = () => {
+  const [checkedItems, setCheckedItems] = useState([false, false]);
+
+  const allChecked = checkedItems.every(Boolean);
+  const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+  // https://v2.chakra-ui.com/docs/components/checkbox#indeterminate
+
+  return (
+    <>
+      <Nav />
+      <Body>
+        <Container>
+          <Flex
+            flexDirection="row"
+            justify="space-between"
             style={{
-              padding: '4px 10px',
+              padding: '29px 35px 28px 47px',
             }}
           >
-            <TagLabel>
-              <Flex>
-                <Image src="/src/assets/icon/info.svg" alt="info" width="14px" marginRight="6px" />
-                <Typography variant="subtitle1">
-                  휴지통에 저장된 항목은 1달이 지나면 자동으로 완전히 삭제됩니다.
-                </Typography>
-              </Flex>
-            </TagLabel>
-          </Tag>
-        </Flex>
-        <NoteList noteType="trash" />
-      </Container>
-    </Body>
-  </>
-);
+            <Typography variant="h1" fontWeight={400}>
+              휴지통
+            </Typography>
+            <Tag
+              size="md"
+              borderRadius="full"
+              variant="outline"
+              colorScheme="blackAlpha"
+              style={{
+                padding: '4px 10px',
+              }}
+            >
+              <TagLabel>
+                <Flex>
+                  <Image src="/src/assets/icon/info.svg" alt="info" width="14px" marginRight="6px" />
+                  <Typography variant="subtitle1">
+                    휴지통에 저장된 항목은 1달이 지나면 자동으로 완전히 삭제됩니다.
+                  </Typography>
+                </Flex>
+              </TagLabel>
+            </Tag>
+          </Flex>
+          <Flex alignItems="center" margin="0 0 1px 8px">
+            <Checkbox
+              padding="16px 14px 16px 17px"
+              isChecked={allChecked}
+              isIndeterminate={isIndeterminate}
+              onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+            />
+            <Stack direction="row" spacing={4}>
+              <Button
+                colorScheme="blackAlpha"
+                variant="outline"
+                height="32px"
+                padding="0 11px"
+                borderColor={styleToken.color.gray200}
+              >
+                <Image src="/src/assets/icon/share.svg" alt="복원" width="16px" marginRight="6px" />
+                <Typography variant="subtitle1">복원</Typography>
+              </Button>
+              <Button
+                colorScheme="blackAlpha"
+                variant="outline"
+                height="32px"
+                padding="0 11px"
+                borderColor={styleToken.color.gray200}
+              >
+                <Image src="/src/assets/icon/trash.svg" alt="영구 삭제" width="16px" marginRight="6px" />
+                <Typography variant="subtitle1">영구 삭제</Typography>
+              </Button>
+            </Stack>
+          </Flex>
+          <NoteList noteType="trash" />
+        </Container>
+      </Body>
+    </>
+  );
+};
 
 const Container = styled.div`
   height: 100%;
